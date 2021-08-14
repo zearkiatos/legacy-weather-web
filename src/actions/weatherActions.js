bd94942c6e4f602e1b5d9bc09ad10a6e77712bfd
zearkiatos <caprilespe@outlook.com>
2021-08-14 10:59:57 -0400

    Ajax Requests with Axios

---
+++ src/actions/weatherActions.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import config from "../config";
+import { WEATHER_TYPES } from "../types";
+
+function fetchWeather(city) {
+  const url = `${config.OPEN_WEATHER_BASE_URL}/forecast?appid=${config.OPEN_WEATHER_API_KEY}&q=${city},us`;
+  const request = axios.get(url);
+  return {
+    type: WEATHER_TYPES.FETCH_WEATHER,
+    payload: request,
+  };
+}
+
+export default {
+  fetchWeather,
+};
