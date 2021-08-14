4f3d1801c4837543c01e97e434bde1a8038dec04
zearkiatos <caprilespe@outlook.com>
2021-08-14 10:28:00 -0400

    Working with APIs

---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+const config = {
+    ENVIRONMENT: process.env.NODE_ENV,
+    OPEN_WEATHER_API_KEY: process.env.OPEN_WEATHER_API_KEY,
+    OPEN_WEATHER_BASE_URL: process.env.OPEN_WEATHER_BASE_URL
+};
+
+export default config;
